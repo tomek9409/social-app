37b9617d62831ec3da999a9208de31b0b7700b97
tomek9409 <kolpak9409@gmail.com>
2022-11-02 13:23:12 +0100

    Etap 2 - poprawka

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,11 +5,12 @@ const Post = (props) => {
   console.log(props.post.user);
   return (
     <div className="post">
-      <div className="postAvatar">
+      <div className="postHeader">
         <img src={props.post.user.avatar_url} alt={props.post.user.username} />
+        <div className="postUser">{props.post.user.username.toUpperCase()}</div>
       </div>
       <div className="postInfo">
-        <div className="postUser">{props.post.user.username.toUpperCase()}</div>
+        <div className="postContent">{props.post.content}</div>
         <div className="postCreated">
           {props.post.user.created_at.slice(0, 10).replaceAll("-", ".")}
         </div>
